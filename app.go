74e5cb33898a1f06829959bbcfbe64497e13b8c4
dwi.prabowo <dwi.prabowo@tokopedia.com>
2017-07-27 09:56:52 +0700

    pointers

--- app.go
+++ app.go
@@ -6,4 +6,24 @@ import (
 
 func main() {
     fmt.Println("~~~", "app.go main")
+    // ==================================================
+    i, j := 42, 2701
+
+    p := &i
+    printVar("p", p)
+    printVar("*p", *p)
+
+    *p = 21
+    printVar("i", i)
+
+    p = &j
+    *p = *p / 37
+    printVar("j", j)
+}
+
+func printVar(varName string, theVar interface{}) {
+    fmt.Print(" ~ " + varName);
+    fmt.Printf("(%T)", theVar);
+    fmt.Print(" : ", theVar);
+    fmt.Println();
 }
