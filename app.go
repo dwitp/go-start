d92f834b8ac68d0f84776afacbdc3a12884f69b0
dwi.prabowo <dwi.prabowo@tokopedia.com>
2017-07-27 15:55:05 +0700

    go tour: struct literals

--- app.go
+++ app.go
@@ -8,9 +8,16 @@ type Vertex struct {
     Y int
 }
 
+var (
+    v1 = Vertex{1, 2}   // has type Vertex
+    v2 = Vertex{X: 1}   // Y:0 is implicit
+    v3 = Vertex{}       // X:0 and Y:0
+    p  = &Vertex{1, 2}  // has type *Vertex
+)
+
 func main() {
-    v := Vertex{1, 2}
-    p := &v
-    p.X = 1e9
-    printVar("v", v)
+    printVar("v1", v1)
+    printVar("p", p)
+    printVar("v2", v2)
+    printVar("v3", v3)
 }
