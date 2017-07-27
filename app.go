4f02f3d040a0e7bba235dcffd47553c92ec223b0
dwi.prabowo <dwi.prabowo@tokopedia.com>
2017-07-27 15:01:18 +0700

    go tour: struct

--- app.go
+++ app.go
@@ -1,31 +1,14 @@
 package main
 
 import (
-    "./pkg/config"
-    "github.com/burntsushi/toml"
-    "time"
 )
 
-func say(s string) {
-    for i := 0; i < 3; i++ {
-        time.Sleep(1000 * time.Millisecond)
-        printVar("s", s)
-    }
+type Vertex struct {
+    X int
+    Y int
 }
 
 func main() {
-    confData, err := FindAndReadConfig("config.toml")
-    if err != nil {
-        printVar("err", err)
-    }
-
-    var config config.Config
-    if _, err := toml.Decode(confData, &config); err != nil {
-        printVar("err", err)
-    }
-
-    printVar("config", config)
-
-    go say("world")
-    say("hello")
+    vertex := Vertex{1, 2}
+    printVar("vertex", vertex)
 }
--- methods.go
+++ methods.go
@@ -1,13 +1,4 @@
 package main
 
 import (
-    "math"
 )
-
-type Vertex struct {
-    X, Y float64
-}
-
-func (v Vertex) Abs() float64 {
-    return math.Sqrt(v.X * v.X + v.Y * v.Y)
-}
