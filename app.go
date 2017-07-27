45d852acb1b74ff95ac1d47326be3a315b77631c
dwi.prabowo <dwi.prabowo@tokopedia.com>
2017-07-27 16:17:07 +0700

    go tour: slices

--- app.go
+++ app.go
@@ -4,13 +4,8 @@ import (
 )
 
 func main() {
-    var a [2]string
-    a[0] = "Hello"
-    a[1] = "World"
-    printVar("a[0]", a[0])
-    printVar("a[1]", a[1])
-    printVar("a", a)
-
     primes := [6]int{2, 3, 5, 7, 11, 13}
-    printVar("primes", primes)
+
+    var s []int = primes[1:4]
+    printVar("s", s)
 }
