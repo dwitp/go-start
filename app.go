12231c8e587bfd9e0e2a92e5b8811b38f7977b6b
dwi.prabowo <dwi.prabowo@tokopedia.com>
2017-07-27 16:05:26 +0700

    go tour: arrays

--- app.go
+++ app.go
@@ -3,21 +3,14 @@ package main
 import (
 )
 
-type Vertex struct {
-    X int
-    Y int
-}
-
-var (
-    v1 = Vertex{1, 2}   // has type Vertex
-    v2 = Vertex{X: 1}   // Y:0 is implicit
-    v3 = Vertex{}       // X:0 and Y:0
-    p  = &Vertex{1, 2}  // has type *Vertex
-)
-
 func main() {
-    printVar("v1", v1)
-    printVar("p", p)
-    printVar("v2", v2)
-    printVar("v3", v3)
+    var a [2]string
+    a[0] = "Hello"
+    a[1] = "World"
+    printVar("a[0]", a[0])
+    printVar("a[1]", a[1])
+    printVar("a", a)
+
+    primes := [6]int{2, 3, 5, 7, 11, 13}
+    printVar("primes", primes)
 }
