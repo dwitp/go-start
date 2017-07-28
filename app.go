c479d16921dfa2610fd05ff0a31f1cfa27aebecd
Dwi Prabowo <dwi.prabowo@tokopedia.com>
2017-07-28 09:55:43 +0700

    How to write go code: Remote packages

--- app.go
+++ app.go
@@ -1,13 +1,9 @@
 package main
 
+import (
+    "github.com/golang/example/stringutil"
+)
+
 func main() {
-    a, b := 1, 2
-    dump(a, b)
-    var c int
-    c = a
-    a = b
-    b = c
-    dump(a, b)
-    a, b = b, a
-    dump(a, b)
+    dump(stringutil.Reverse("!dlroW olleH"))
 }
