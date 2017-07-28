2eae4f0a6dee099969bfb5a37d2fa124d25df170
Dwi Prabowo <dwi.prabowo@tokopedia.com>
2017-07-28 14:50:29 +0700

    A Tour of Go: Creating a slice with make

--- app.go
+++ app.go
@@ -5,9 +5,19 @@ import (
 )
 
 func main() {
-    var s[]int
-    u.D(s, len(s), cap(s))
-    if s == nil {
-        u.D("nil!")
-    }
+    s := make([]int, 5)
+    u.D("s")
+    u.S(s)
+
+    b := make([]int, 0, 5)
+    u.D("b")
+    u.S(b)
+
+    c := b[:2]
+    u.D("c")
+    u.S(c)
+
+    d := c[2:5]
+    u.D("d")
+    u.S(d)
 }
