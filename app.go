63a98e6135e1cec5809d27c5a4b483b59bc57563
Dwi Prabowo <dwi.prabowo@tokopedia.com>
2017-07-28 08:02:34 +0700

    swap values

--- app.go
+++ app.go
@@ -1,9 +1,13 @@
 package main
 
-import (
-    "github.com/dwitp/stringutil"
-)
-
 func main() {
-    dump(stringutil.Reverse("!oG ,olleH"))
+    a, b := 1, 2
+    dump(a, b)
+    var c int
+    c = a
+    a = b
+    b = c
+    dump(a, b)
+    a, b = b, a
+    dump(a, b)
 }
