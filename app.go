bcbcc6f947f87b8e4884c2fa45d8dba0770911d7
Dwi Prabowo <dwi.prabowo@tokopedia.com>
2017-07-28 14:14:10 +0700

    A Tour of Go: Slice length and capacity

--- app.go
+++ app.go
@@ -1,18 +1,26 @@
 package main
 
 import (
-    "github.com/dwitp/golang/src/u"
+    "fmt"
 )
 
 func main() {
     s := []int{2, 3, 5, 7, 11, 13}
+    printSlice(s)
 
-    s = s[1:4]
-    u.D("s", s)
+    // Slice the slice to give it zero length
+    s = s[:0]
+    printSlice(s)
 
-    s = s[:2]
-    u.D("s", s)
+    // Extend its length
+    s = s[:4]
+    printSlice(s)
 
-    s = s[1:]
-    u.D("s", s)
+    // Drop its first two values
+    s = s[2:]
+    printSlice(s)
+}
+
+func printSlice(s []int) {
+    fmt.Printf("len=%d cap=%d %v\n", len(s), cap(s), s)
 }
