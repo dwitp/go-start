f4331618c931b562c6b00d8234e406734db75a19
Dwi Prabowo <dwi.prabowo@tokopedia.com>
2017-07-28 14:55:24 +0700

    A Tour of Go: Slices of slices

--- app.go
+++ app.go
@@ -2,22 +2,25 @@ package main
 
 import (
     "github.com/dwitp/golang/src/u"
+    "strings"
 )
 
 func main() {
-    s := make([]int, 5)
-    u.D("s")
-    u.S(s)
+    // Create a tic-tac-toe board.
+    board := [][]string{
+        []string{"_", "_", "_"},
+        []string{"_", "_", "_"},
+        []string{"_", "_", "_"},
+    }
 
-    b := make([]int, 0, 5)
-    u.D("b")
-    u.S(b)
+    // The players take turns.
+    board[0][0] = "X"
+    board[2][2] = "O"
+    board[1][2] = "X"
+    board[1][0] = "O"
+    board[0][2] = "X"
 
-    c := b[:2]
-    u.D("c")
-    u.S(c)
-
-    d := c[2:5]
-    u.D("d")
-    u.S(d)
+    for i := 0; i < len(board); i++ {
+        u.D(strings.Join(board[i], " "))
+    }
 }
