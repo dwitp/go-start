ee80b7898d0044f558ce5f35d5ca75f7b71ebd48
Dwi Prabowo <dwi.prabowo@tokopedia.com>
2017-07-28 13:55:29 +0700

    A Tour of Go: Slice literals

--- app.go
+++ app.go
@@ -2,9 +2,25 @@ package main
 
 import (
     "github.com/dwitp/golang/src/u"
-    "github.com/golang/example/stringutil"
 )
 
 func main() {
-    u.D(stringutil.Reverse("!dlroW olleH"), "test", "jos")
+    q := []int{2, 3, 5, 7, 11, 13}
+    u.D("q", q)
+
+    r := []bool{true, false, true, true, false, true}
+    u.D("r", r)
+
+    s := []struct{
+        i int
+        b bool
+    }{
+        {2, true},
+        {3, false},
+        {5, true},
+        {7, true},
+        {11, false},
+        {13, true},
+    }
+    u.D("s", s)
 }
