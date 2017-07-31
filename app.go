f92fccd99149ede41dbab5bfc0144a418bfa05c4
Dwi Prabowo <dwi.prabowo@tokopedia.com>
2017-07-31 08:19:36 +0700

    A Tour of Go: Function closures

--- app.go
+++ app.go
@@ -2,18 +2,19 @@ package main
 
 import (
     "github.com/dwitp/golang/src/u"
-    "math"
 )
 
-func compute(fn func(float64, float64) float64) float64 {
-    return fn(3, 4)
+func adder() func(int) int {
+    sum := 0
+    return func(x int) int {
+        sum += x
+        return sum
+    }
 }
 
 func main() {
-    hypot := func(x, y float64) float64 {
-        return math.Sqrt(x*x + y*y)
+    pos, neg := adder(), adder()
+    for i := 0; i < 10; i++ {
+        u.D(pos(i), neg(-2*i))
     }
-    u.D(hypot(5, 12))
-    u.D(compute(hypot))
-    u.D(compute(math.Pow))
 }
